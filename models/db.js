95946bcf11123959856129ddf54645276b3dc512
souvik <souvikaasds01@gmail.com>
2021-09-08 22:32:25 +0530

    cloud mongodb URL added

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/FoodArt',{
+mongoose.connect('mongodb+srv://souvik:mongo%402002@foodart.skzfr.mongodb.net/test?authSource=admin&replicaSet=atlas-9c4ltn-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',{
      useNewUrlParser:true,
      useUnifiedTopology:true
 }).then(()=>{
